779f091f383a4383698c215e3fee16de5a50899e
oliver-itc <oliver-itc@hushmail.com>
2013-08-31 00:32:11 -0400

    add operator exercise project

---
+++ OperatorExercise/OperatorExercise.cpp
@@ -0,0 +1,18 @@
+/*
+
+	Operator Exercise
+
+*/
+
+#include "../../std_lib_facilities.h"
+
+int main()
+{
+	cout << " Please enter a floating point value: ";
+	double n;
+	cin >> n;
+	cout << "n == " << n
+		<< "\nn+1 == " << n+1
+		<< endl;
+
+}
